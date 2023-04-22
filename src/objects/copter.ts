796a00c9a8f6782223c42476699a3ecb2f08d3c3
S3-D1 <kegeldiddi@outlook.de>
2023-04-22 15:16:29 +0200

    no inputs on game over

--- src/objects/copter.ts
+++ src/objects/copter.ts
@@ -38,7 +38,7 @@ export class Copter extends Phaser.GameObjects.Image {
 
     public update() {
         super.update();
-        if (this.jumpKey.isDown || this.scene.input.activePointer.isDown) {
+        if (!this.isCrashed && (this.jumpKey.isDown || this.scene.input.activePointer.isDown)) {
             this.body.setVelocityY(-200);
         }
     }
