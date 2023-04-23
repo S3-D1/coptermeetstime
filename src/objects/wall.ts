d3ab8ea63934e3a71230fd649ad8539aece6704a
S3-D1 <kegeldiddi@outlook.de>
2023-04-23 20:05:07 +0200

    improve wall handling

--- src/objects/wall.ts
+++ src/objects/wall.ts
@@ -4,7 +4,7 @@ import Body = Phaser.Physics.Arcade.Body;
 export class Wall extends Phaser.GameObjects.Image {
     body: Phaser.Physics.Arcade.Body;
 
-    public static readonly defaultHeight = 150;
+    public static readonly defaultHeight = 140;
 
     constructor(aParams: IImageConstructor) {
         super(
